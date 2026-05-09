584fefce2b28e994acbd5fcabe0e6ffba83334c6
Nikita <nikitareznikov2606@gmail.com>
2026-05-09 20:49:23 +0300

    added config

--- Lab_8/proxy.js
+++ Lab_8/proxy.js
@@ -3,3 +3,11 @@ const axios = require('axios');
 
 const app = express();
 app.use(express.json());
+
+const API_URL = 'https://jsonplaceholder.typicode.com';
+
+let authConfig = {
+    type: 'API_KEY',
+    apiKey: 'my-secret-api-key',
+    token: 'initial-token'
+};
